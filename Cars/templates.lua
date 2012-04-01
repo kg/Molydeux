407a91b5be2615c9239f5d52e9be476837f0d765
Kevin Gadd <kevin.gadd@gmail.com>
2012-03-31 21:35:56 -0700

    Add the other cars

--- Cars/templates.lua
+++ Cars/templates.lua
@@ -2,5 +2,13 @@ return {
     {
         image = "Art/Game/carA.png";
         size = {128, 128};
+    },
+    {
+        image = "Art/Game/busA.png";
+        size = {128, 128};
+    },
+    {
+        image = "Art/Game/busB.png";
+        size = {128, 128};
     }
 }
