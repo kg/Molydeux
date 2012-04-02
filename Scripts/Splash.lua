a21b932e7530d88f015be0c7d0da44cf31857ed6
Brandon Dillon <cbdillon@gmail.com>
2012-04-01 17:49:31 -0700

    New splash

--- Scripts/Splash.lua
+++ Scripts/Splash.lua
@@ -1,3 +1,5 @@
+local Util = require('Scripts.Util')
+
 local Splash = {}
 Splash.__index = Splash
 
@@ -9,49 +11,13 @@ function Splash.new()
 
     -- Create a layer and add it as a render pass
     Ob.layer = MOAILayer2D.new()
-   
-    -- Load the splash image
-    local gfxQuad = MOAIGfxQuad2D.new()
-    gfxQuad:setTexture('Art/Splash/Splash.png')
-    gfxQuad:setRect(0, 0, 1024, 768)
-    gfxQuad:setUVRect(0, 0, 1, 1)
-   
-    -- Create a prop for the background art
-    local splash = MOAIProp2D.new()
-    splash:setDeck(gfxQuad)
-        
-    -- Create the splash font
-    local font = MOAIFont.new()    
-    local charcodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,:;!?()&/-'
-    font:loadFromTTF('Art/Fonts/AllenconDemo.ttf', charcodes)
-        
-    -- Create a large font style
-    local titleStyle = MOAITextStyle.new()
-    titleStyle:setFont(font)
-    titleStyle:setSize(76)
+       
+    Ob.splash01 = Util.makeSimpleProp('Art/Splash/title01.png', nil, nil, true)
+    Ob.splash02 = Util.makeSimpleProp('Art/Splash/title02.png', nil, nil, true)
+    Ob.splash03 = Util.makeSimpleProp('Art/Splash/title03.png', nil, nil, true)
     
-    local promptStyle = MOAITextStyle.new()
-    promptStyle:setFont(font)
-    promptStyle:setSize(24)
-    
-    -- Initialize the title text box    
-    local titleTextBox = MOAITextBox.new ()
-    titleTextBox:setString("<c:0>Promiscuous Pigeon<c>")
-    titleTextBox:setStyle(titleStyle)
-    titleTextBox:setRect(50, 0, 1024, 230)
-    titleTextBox:setAlignment(MOAITextBox.LEFT_JUSTIFY, MOAITextBox.CENTER_JUSTIFY)
-
-    -- Initialize the prompt text box
-    local promptTextBox = MOAITextBox.new()
-    promptTextBox:setString("<c:0>press anywhere<c>")
-    promptTextBox:setStyle(promptStyle)
-    promptTextBox:setRect(200, 500, 1024, 800)
-    promptTextBox:setAlignment(MOAITextBox.LEFT_JUSTIFY, MOAITextBox.CENTER_JUSTIFY)    
-
     -- Add the objects to our layer
-    Ob.layer:insertProp(splash)
-    Ob.layer:insertProp(titleTextBox)
-    Ob.layer:insertProp(promptTextBox)
+    Ob.layer:insertProp(Ob.splash01)
    
     -- Give the object to the caller
     return Ob
@@ -61,7 +27,6 @@ end
 function Splash:run(viewport)
     viewport:setSize(1024, 768)
     viewport:setScale(1024, -768)
-    viewport:setOffset(-1, 1)
     
     self.layer:setViewport(viewport)
     MOAISim.pushRenderPass(self.layer)
